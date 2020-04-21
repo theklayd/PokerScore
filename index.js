e24d620f95f22ea0b259f05ed7a5e6e12b30eb23
Clyde Santos <clydesantos1028@gmail.com>
2020-04-21 21:24:51 +0800

    port 3000 to 80
    80 is default port by heroku

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const sortBy = require('sort-array');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
