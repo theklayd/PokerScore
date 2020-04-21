b3ad639e9231a2696f9d9551bd3290fa669c5edb
Clyde Santos <clydesantos1028@gmail.com>
2020-04-21 21:36:00 +0800

    process.env.PORT || 3000;

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const sortBy = require('sort-array');
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
