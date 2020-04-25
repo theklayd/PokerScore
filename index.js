2bd5a1138646327d0d10e680edca9c33a02615b4
Clyde Santos <clydesantos1028@gmail.com>
2020-04-25 18:10:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+  res.header("Access-Control-Expose-Headers", "ETag");
+  
   // Request methods you wish to allow
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,OPTIONS');
   // Request headers you wish to allow
